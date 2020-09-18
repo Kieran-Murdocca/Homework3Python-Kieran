0b00734b3fe9dcc02e1099af023fe681d5679f50
KieranMurdocca <kkm5754@psu.edu>
2020-09-18 15:59:21 +0000

    first check

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Author: Kieran Murdocca kkm5754@psu.edu
 # Collaborator: Peter Schulman pks5481@psu.edu
+# Section 004
+# Homework 3
 
 def digit_sum(n): 
   if n==0: 
     return 0
   elif n<0: 
     return "out of domain."
+
   return (n % 10 + digit_sum(int(n/10)))
 
 def run(): 
